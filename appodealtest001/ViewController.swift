59ae67d029df14864201b39f1da3409c9c3b872e
Raymond Lam <raymondlam1991@gmail.com>
2018-03-09 15:26:31 -0600

    Initial Commit

---
+++ appodealtest001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appodealtest001
+//
+//  Created by Raymond Lam on 3/9/18.
+//  Copyright © 2018 Raymond Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
